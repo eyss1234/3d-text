7c3c2134354ea8848aee188774541c60573edf01
Emily Santos <eyssantos@gmail.com>
2023-08-16 16:57:44 +0100

    Refactor to use one material to entire scene

--- src/script.js
+++ src/script.js
@@ -58,17 +58,16 @@ fontLoader.load(
         //     - (textGeometry.boundingBox.max.z - 0.03) * 0.5  // Subtract bevel thickness
         //   )
       textGeometry.center()
-      const textMaterial = new THREE.MeshMatcapMaterial({matcap: matcapTexture})
-      textMaterial.wireframe = false
-      const text = new THREE.Mesh(textGeometry, textMaterial)
+      const material = new THREE.MeshMatcapMaterial({matcap: matcapTexture})
+      material.wireframe = false
+      const text = new THREE.Mesh(textGeometry, material)
       scene.add(text)
 
       // Creating donuts
       const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
-      const donutMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
 
       for(let i = 0; i < 100; i++) {       
-        const donut = new THREE.Mesh(donutGeometry, donutMaterial)
+        const donut = new THREE.Mesh(donutGeometry, material)
         
         donut.position.x = (Math.random() - 0.5) * 10
         donut.position.y = (Math.random() - 0.5) * 10
