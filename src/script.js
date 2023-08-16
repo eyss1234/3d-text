e49039201b1f14a6f1179cb145bff0bc024ee16e
Emily Santos <eyssantos@gmail.com>
2023-08-16 17:04:06 +0100

    Increased amount of torus geometry and space range

--- src/script.js
+++ src/script.js
@@ -66,12 +66,12 @@ fontLoader.load(
       // Creating donuts
       const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
 
-      for(let i = 0; i < 100; i++) {       
+      for(let i = 0; i < 200; i++) {       
         const donut = new THREE.Mesh(donutGeometry, material)
         
-        donut.position.x = (Math.random() - 0.5) * 10
-        donut.position.y = (Math.random() - 0.5) * 10
-        donut.position.z = (Math.random() - 0.5) * 10
+        donut.position.x = (Math.random() - 0.5) * 15
+        donut.position.y = (Math.random() - 0.5) * 15
+        donut.position.z = (Math.random() - 0.5) * 15
 
         donut.rotation.x = Math.random() * Math.PI
         donut.rotation.y = Math.random() * Math.PI
